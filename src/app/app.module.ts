c2b2cfcc333dcd13c9a999e947d346da2e61625b
Kusuma Gottipati <kusumagottipati06@gmail.com>
2024-07-10 21:41:00 +0530

    feature-home-screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginpageComponent } from './loginpage/loginpage.component';
+import { FeatureHomeScreenComponent } from './feature-home-screen/feature-home-screen.component';
 import { HeaderComponent } from './header/header.component';
-
+import { LoginpageComponent } from './loginpage/loginpage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginpageComponent,
-    HeaderComponent
+    HeaderComponent,
+    FeatureHomeScreenComponent
   ],
   imports: [
     BrowserModule,
