3dae3b16462cc4518d2e1e79187b2eaa39994026
gaddam sarayu <gaddamsarayu@gmail.com>
2024-07-12 22:52:40 +0530

    add featureregister

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FeatureRegisterComponent } from './feature-register/feature-register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FeatureRegisterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/feature-register/feature-register.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-feature-register',
+  templateUrl: './feature-register.component.html',
+  styleUrls: ['./feature-register.component.css']
+})
+export class FeatureRegisterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
